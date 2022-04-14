56ee4f8af8657a8957c1d03f1ad046f2c8d5ea01
David <rodpe.david@gmail.com>
2022-04-14 14:15:12 -0500

    like and fav buttons with local storage

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import {Link as LinkRouter} from 'react-router-dom';
 import styled from 'styled-components'
+import { useLocalStorage } from '../hooks/useLocalStorage';
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
+import {AiFillStar, AiOutlineStar} from 'react-icons/ai'
 
 const AnimeCard = ({anime, id}) => {
+
+  // Likes and favs
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const starKey = `star-${id}`
+  const [favorite, setFavorite] = useLocalStorage(starKey, false)
+  const IconStar = favorite ? AiFillStar : AiOutlineStar
+
+  // const [star, setStar] = useState(second)
+
+
   return (
     <CardContainer className='hoverwrap'>
       <img src = {anime.attributes.posterImage.small} alt = "anime poster" style={{objectFit:'cover'}}/>
       <div className='hovercap'>
         <LinkRouter className='linkTitle' to = {`/anime/${id}`}>{anime.attributes.titles.en_jp}</LinkRouter>
         <p>Favorites: {anime.attributes.favoritesCount} </p>
+
+        <button onClick={()=>setLiked(!liked)}>
+          <Icon size='32px'/> {anime.attributes.favoritesCount} likes!
+        </button>
+
+        <button onClick={()=>setFavorite(!favorite)}>
+          <IconStar size = '32px'>{anime.attributes.favoritesCount}</IconStar>
+        </button>
+
       </div>
     </CardContainer>
   )
